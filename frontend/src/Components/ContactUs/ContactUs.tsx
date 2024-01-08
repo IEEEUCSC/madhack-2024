4a0526862d1cb952f5333397b951802ab101a53f
lakshith <lakshith.k.nishshanke@gmail.com>
2024-01-08 11:52:36 +0530

    Add contact us numbers

--- frontend/src/Components/ContactUs/ContactUs.tsx
+++ frontend/src/Components/ContactUs/ContactUs.tsx
@@ -15,44 +15,33 @@ function ContactUS() {
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-md-5 ml-auto aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+                    <div className="col-lg-4 col-sm-6 ml-auto aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
                         <div className="p-4 mb-3">
-                            <p className="mb-0 font-weight-bold text-secondary text-uppercase mb-3">Organizing Committee Vice President</p>
-                            <p className="mb-4">Tikki tikiri tikiri liya</p>
-                            <p className="mb-0 text-secondary mb-3">Phone</p>
-                            <p className="mb-4"><a href="#">+1 232 3235 324</a></p>
-                            <p className="mb-0 text-secondary mb-3">Email Address</p>
-                            <p className="mb-0"><a href="#">youremail@domain.com</a></p>
+                            <p className="mb-3 font-weight-bold">Savini Rathnayake</p>
+                            <p className="mb-0 text-secondary">Phone</p>
+                            <p className="mb-3"><a>+94 71 926 1699</a></p>
+                            <p className="mb-0 text-secondary">Email Address</p>
+                            <p className="mb-0"><a href="mailto:savini.ur@gmail.com">savini.ur@gmail.com</a></p>
                         </div>
                     </div>
-                    <div className="col-md-5 ml-auto aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+
+                    <div className="col-lg-4 col-sm-6 ml-auto aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
                         <div className="p-4 mb-3">
-                            <p className="mb-0 font-weight-bold text-secondary text-uppercase mb-3">Organizing Committee Vice President</p>
-                            <p className="mb-4">Tikki tikiri tikiri liya</p>
-                            <p className="mb-0 text-secondary mb-3">Phone</p>
-                            <p className="mb-4"><a href="#">+1 232 3235 324</a></p>
-                            <p className="mb-0 text-secondary mb-3">Email Address</p>
-                            <p className="mb-0"><a href="#">youremail@domain.com</a></p>
+                            <p className="mb-3 font-weight-bold">Noji Yudara</p>
+                            <p className="mb-0 text-secondary">Phone</p>
+                            <p className="mb-3"><a>+94 70 351 7563</a></p>
+                            <p className="mb-0 text-secondary">Email Address</p>
+                            <p className="mb-0"><a href="mailto:noji.yu123@gmail.com">noji.yu123@gmail.com</a></p>
                         </div>
                     </div>
-                    <div className="col-md-5 ml-auto aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+
+                    <div className="col-lg-4 col-sm-6 ml-auto aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
                         <div className="p-4 mb-3">
-                            <p className="mb-0 font-weight-bold text-secondary text-uppercase mb-3">Organizing Committee Vice President</p>
-                            <p className="mb-4">Tikki tikiri tikiri liya</p>
-                            <p className="mb-0 text-secondary mb-3">Phone</p>
-                            <p className="mb-4"><a href="#">+1 232 3235 324</a></p>
-                            <p className="mb-0 text-secondary mb-3">Email Address</p>
-                            <p className="mb-0"><a href="#">youremail@domain.com</a></p>
-                        </div>
-                    </div>
-                    <div className="col-md-5 ml-auto aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
-                        <div className="p-4 mb-3">
-                            <p className="mb-0 font-weight-bold text-secondary text-uppercase mb-3">Organizing Committee Vice President</p>
-                            <p className="mb-4">Tikki tikiri tikiri liya</p>
-                            <p className="mb-0 text-secondary mb-3">Phone</p>
-                            <p className="mb-4"><a href="#">+1 232 3235 324</a></p>
-                            <p className="mb-0 text-secondary mb-3">Email Address</p>
-                            <p className="mb-0"><a href="#">youremail@domain.com</a></p>
+                            <p className="mb-3 font-weight-bold">Likitha Chathubhashini</p>
+                            <p className="mb-0 text-secondary">Phone</p>
+                            <p className="mb-3"><a>+94 76 698 8362</a></p>
+                            <p className="mb-0 text-secondary">Email Address</p>
+                            <p className="mb-0"><a href="mailto:likithachathu@gmail.com">likithachathu@gmail.com</a></p>
                         </div>
                     </div>
                 </div>
